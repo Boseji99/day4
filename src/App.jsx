a190caba8fd6e50d7aa1b1e1bde4b96cb97cff5f
Sumon Bose <boseji99@gamil.com>
2024-12-22 16:18:31 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/vite.svg 	renamed:    project_new/day4 -> src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/components/CardComponent.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   src/utility/data.js 	new file:   tailwind.config.js 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardComponent from './components/CardComponent'
+
+const App = () => {
+  return (
+   <div>
+    <CardComponent />
+   </div>
+  )
+}
+
+export default App
